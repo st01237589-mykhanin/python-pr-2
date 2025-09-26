9735a6f66319c62a7a46adf96493d11072375737
Maksym Khanin <st01237589@stud.duikt.edu.ua>
2025-09-26 08:19:08 +0300

    Add CsvToJsonConverter in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from CsvReader import CsvReader
+from CsvToJsonConverter import CsvToJsonConverter
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
@@ -6,3 +7,9 @@ if __name__ == "__main__":
     data = csv_reader.read_data(csv_url)
     for row in data:
         print(row)
+    
+    csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
+    json_path = "students_data.json"  # Шлях до файлу, куди буде збережено JSON
+
+    converter = CsvToJsonConverter()
+    converter.read_and_convert(csv_url, json_path)
